9a0475dae824be0d658f6bfc4ec946221f8e1114
egchengg <egc5zc@virginia.edu>
2022-09-23 10:32:00 -0400

    Initial commit

---
+++ Assets/Scripts/BrokenScripts/10/MoveObject10.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject10 : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector3 moveDirection;
+
+    float totalMoveDistance;
+    Vector3 startingLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalMoveDistance = 10f;
+        startingLocation = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceTraveled = GetDistanceTraveled();
+
+        if (distanceTraveled > totalMoveDistance)
+        {
+            FlipMoveDirection();
+        }
+
+        gameObject.transform.Translate(moveDirection * moveSpeed);
+    }
+
+    void FlipMoveDirection()
+    {
+        moveDirection = -moveDirection;
+        startingLocation = gameObject.transform.position;
+    }
+
+    float GetDistanceTraveled()
+    {
+        return Vector3.Distance(gameObject.transform.position, startingLocation);
+    }
+}
